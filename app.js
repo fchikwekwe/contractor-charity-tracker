2ef5e844d8dd93329dca9264d8e5876dd266272a
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2018-10-03 12:40:08 -0700

    added index view

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+var exphbs = require('express-handlebars');
+
+mongoose.connect('mongodb://localhost/charity-tracker', { useMongoClient: true });
+
+app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+
 
 app.get('/', (req, res) => {
-  res.send('test')
+  res.render('index');
 })
 
 app.listen(3000, () => {
