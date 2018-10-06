86f535badbaae2a5b650104ab185945642a7598b
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2018-10-06 14:28:47 -0700

    added test coverage for all events routes

---
+++ test/test-events.js
@@ -0,0 +1,114 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../app');
+const should = chai.should();
+const Event = require('../models/event');
+
+const sampleEvent = {
+    "title": "Great Cause Benefit",
+    "charity": "Great Cause Fund",
+    "notes": "great event. gave lots of money."
+}
+
+chai.use(chaiHttp);
+
+describe('Events', () => {
+
+    after(() => {
+        Event.deleteMany({title: 'Great Cause Benefit'}).exec((err, events) =>{
+            console.log(events)
+            events.remove();
+        })
+    });
+
+    // Test events index
+    it('should index ALL events on / GET', (done) => {
+        chai.request(server)
+            .get('/')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.should.be.html;
+                done();
+            });
+    });
+
+    // Test events new route
+    it('should display new form on /events/new GET', (done) => {
+        chai.request(server)
+            .get(`/events/new`)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.should.be.html
+                    done();
+                });
+    });
+
+    // Test events show route
+    it('should show a single event on /events/<id> GET', (done) => {
+        var event = new Event(sampleEvent);
+        event.save((err, data) => {
+            chai.request(server)
+                .get(`/events/${data._id}`)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.should.be.html
+                    done();
+                });
+        });
+    });
+
+    // Test events edit route
+    it('should edit a single event on /events/<id>/edit GET', (done) => {
+        var event = new Event(sampleEvent);
+        event.save((err, data) => {
+            chai.request(server)
+                .get(`/events/${data._id}/edit`)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.should.be.html
+                    done();
+                });
+        });
+    });
+
+    // Test events create route
+    it('should create a single event on /events POST', (done) => {
+        chai.request(server)
+            .post('/events')
+            .send(sampleEvent)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.should.be.html
+                done();
+            });
+    });
+
+    // Test events update route
+    it('should update a single event on /events/<id> PUT', (done) => {
+        var event = new Event(sampleEvent);
+        event.save((err, data) => {
+            chai.request(server)
+            .put(`/events/${data._id}?_method=PUT`)
+            .send({'title': 'Updating the title'})
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.should.be.html
+                done();
+            });
+        });
+    });
+
+    //Test events delete route
+    it('should delete a single event on /events/<id> DELETE', (done) => {
+        var event = new Event(sampleEvent);
+        event.save((err, data) => {
+            chai.request(server)
+            .delete(`/events/${data._id}?_method=DELETE`)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.should.be.html
+                done();
+            });
+        });
+    });
+})
